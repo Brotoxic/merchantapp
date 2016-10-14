5b319f4a88d9f6480e795a1a6aa418e1f89a7ac7
Alexandre LE CORRE <toxicportfolio@gmail.com>
2016-10-14 22:42:48 +0200

    Enfi

--- www/js/config.js
+++ www/js/config.js
@@ -2,6 +2,6 @@
 var krms_config ={			
 	'ApiUrl' : "https://www.sosresto.com/merchantapp/api",	
 	'DialogDefaultTitle' : "SOS Resto",
-	'pushNotificationSenderid' : "AIzaSyC4o1bvitIWAOV3EWD-FhCDPQQi1WTD1e0",
-	'APIHasKey' : ""
+	'pushNotificationSenderid' : "564106818680",
+	'APIHasKey' : "809e604443eb4aa7f2d27acc28f6ff1a"
 };
