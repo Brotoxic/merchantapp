037a2d0676618197f41f343a6ad22716839fa947
Alexandre LE CORRE <toxicportfolio@gmail.com>
2016-10-14 23:39:11 +0200

    dssdsd

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={			
-	'ApiUrl' : "https://www.sosresto.com/merchantapp/api",	
+	'ApiUrl' : "http://www.sosresto.com/merchantapp/api",	
 	'DialogDefaultTitle' : "SOS Resto",
 	'pushNotificationSenderid' : "564106818680",
-	'APIHasKey' : ""
+	'APIHasKey' : "1"
 };
