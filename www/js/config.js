0780aa9ed27b7e7f42a28d19942adf7053dd3cb9
Alexandre LE CORRE <toxicportfolio@gmail.com>
2016-10-14 18:16:38 +0200

    changement api

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={			
-	'ApiUrl' : "http://sosresto.com/merchantapp/api",	
+	'ApiUrl' : "https://www.sosresto.com/merchantapp/api",	
 	'DialogDefaultTitle' : "SOS Resto",
 	'pushNotificationSenderid' : "AIzaSyC4o1bvitIWAOV3EWD-FhCDPQQi1WTD1e0",
 	'APIHasKey' : "3a26b2a3100f43b99b72bbf4a15fce43"
