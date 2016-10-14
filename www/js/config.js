152484ffea5d0d017fb58a0c8416529f51918970
Alexandre LE CORRE <toxicportfolio@gmail.com>
2016-10-14 22:24:02 +0200

    Try to config

--- www/js/config.js
+++ www/js/config.js
@@ -3,5 +3,5 @@ var krms_config ={
 	'ApiUrl' : "https://www.sosresto.com/merchantapp/api",	
 	'DialogDefaultTitle' : "SOS Resto",
 	'pushNotificationSenderid' : "AIzaSyC4o1bvitIWAOV3EWD-FhCDPQQi1WTD1e0",
-	'APIHasKey' : "3a26b2a3100f43b99b72bbf4a15fce43"
+	'APIHasKey' : ""
 };
